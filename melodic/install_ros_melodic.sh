c90145da842bbe3983c407213d6407091b3b85f4
Liews Wuttipat <sweilz.w@gmail.com>
2019-06-18 19:15:38 +0700

    add key

--- melodic/install_ros_melodic.sh
+++ melodic/install_ros_melodic.sh
@@ -26,13 +26,13 @@ sudo ntpdate ntp.ubuntu.com
 
 echo "[Add the ROS repository]"
 if [ ! -e /etc/apt/sources.list.d/ros-lastest.list ]; then
-    sudo sh -c "echo \"deb http://packages.ros.org/ros/ubuntu ${name_os_version} main\" > /etc/apt/sources.list.d/ros-latest.list"
+    sudo sh -c "echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list"
 fi
 
 echo "[Download the ROS keys]"
 roskey=`apt-key list | grep "ROS Builder"`
 if [ -z "$roskey" ]; then
-    sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+    sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
 fi
 
 echo "[Check the ROS keys]"
